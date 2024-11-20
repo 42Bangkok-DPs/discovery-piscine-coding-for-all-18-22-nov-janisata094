c94127212429a79bd716ee5a653a377d6189e92a
janisata094 <67030038@kmitl.ac.th>
2024-11-20 06:09:27 +0000

    to25.py

---
+++ cell03/ex00/ to25.py
@@ -0,0 +1,15 @@
+number = input("Enter a number less than 25\n").strip()
+
+try:
+    number = int(number)
+
+    if number > 25:
+        print("Error")
+    else:
+        
+        while number <= 25:
+            print(f"Inside the loop, my variable is {number}")
+            number += 1
+
+except ValueError:
+    print("That's not a valid number.")
