c4e4e412bbb89be261531920fd7162f88ae85527
janisata094 <67030038@kmitl.ac.th>
2024-11-20 07:06:53 +0000

    round_up.py

---
+++ cell04/ex04/round_up.py
@@ -0,0 +1,12 @@
+import math
+user_input = input("Give me a number: ")
+
+try:
+  
+    number = float(user_input)
+    
+    rounded_number = math.ceil(number)
+    
+    print(rounded_number)
+except ValueError:
+    print("Invalid input. Please enter a valid number.")
